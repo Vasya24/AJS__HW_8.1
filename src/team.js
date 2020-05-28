2228ab108573c627db5a8cac84f6faba89477a73
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-05-28 11:34:39 +0300

    Done'

---
+++ src/team.js
@@ -0,0 +1,17 @@
+class Team {
+    constructor() {
+        this.members = new Set();
+    }
+    add(Character) {
+      this.members.add(Character)
+    }
+  addAll(...Character) {
+    this.members.add(Character)
+ 
+  }
+  toArray() {
+    this.members = Array.from(this.members)
+  }
+}
+
+export { Team }
